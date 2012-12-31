8f61dcd6a6c021dad0f48a45721d91bc54f00924
Unpakt <unpakt@darkknight.local>
2012-12-31 10:48:12 -0500

    Adding average rating to businesses

--- lib/providers/citysearch_reviews/business.rb
+++ lib/providers/citysearch_reviews/business.rb
@@ -1,13 +1,14 @@
 module Providers
   module CitysearchReviews
     class Business
-      attr_accessor :name, :address, :reference
+      attr_accessor :name, :address, :reference, :average_rating
 
       def initialize(node)
         @name = node["name"]
         full_address = node["address"]
         @address = [full_address["street"], full_address["city"], full_address["state"], full_address["postal_code"]].join(" ")
         @reference = node["id"]
+        @average_rating = node["rating"]
       end
     end
   end
--- lib/providers/google_reviews/business.rb
+++ lib/providers/google_reviews/business.rb
@@ -1,12 +1,13 @@
 module Providers
   module GoogleReviews
     class Business
-      attr_accessor :name, :address, :reference
+      attr_accessor :name, :address, :reference, :average_rating
 
       def initialize(node)
         @name = node["name"]
         @address = node["formatted_address"]
         @reference = node["reference"]
+        @average_rating = node["rating"]
       end
     end
   end
