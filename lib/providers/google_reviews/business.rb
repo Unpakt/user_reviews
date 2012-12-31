981313e62bcaee079fa4f767eddc9a1a10edea08
Unpakt <unpakt@darkknight.local>
2012-12-31 15:57:50 -0500

    adding id to google reviews object

--- lib/providers/google_reviews/business.rb
+++ lib/providers/google_reviews/business.rb
@@ -1,13 +1,14 @@
 module Providers
   module GoogleReviews
     class Business
-      attr_accessor :name, :address, :reference, :average_rating
+      attr_accessor :name, :address, :reference, :average_rating, :id
 
       def initialize(node)
         @name = node["name"]
         @address = node["formatted_address"]
         @reference = node["reference"]
         @average_rating = node["rating"]
+        @id = node["id"]
       end
     end
   end
