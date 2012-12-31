a18180b33809236e30a3579134abb6c20681f3e4
Unpakt <unpakt@darkknight.local>
2012-12-31 16:27:47 -0500

    Store the associated google plus url

--- lib/providers/google_reviews/business.rb
+++ lib/providers/google_reviews/business.rb
@@ -1,15 +1,14 @@
 module Providers
   module GoogleReviews
     class Business
-      attr_accessor :name, :address, :reference, :average_rating, :id
+      attr_accessor :name, :address, :reference, :average_rating, :url
 
       def initialize(node)
         @name = node["name"]
         @address = node["formatted_address"]
         @reference = node["reference"]
         @average_rating = node["rating"]
-        node["url"] =~ /https?:\/\/plus.google.com\/(\d+)(\/.*)?/
-        @id = $1
+        @url = node["url"]
       end
     end
   end
