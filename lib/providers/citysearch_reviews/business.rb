ca4d07924859242ecc3857fcd8178e9e37fe8d6a
Unpakt <unpakt@darkknight.local>
2013-01-03 17:58:57 -0500

    Fixed city search api parsing

--- lib/providers/citysearch_reviews/business.rb
+++ lib/providers/citysearch_reviews/business.rb
@@ -4,7 +4,7 @@ module Providers
       attr_accessor :name, :address, :reference, :average_rating
 
       def self.build_business(node)
-        return nil if [node["name"], node["address"], node["rating"], node["id"]].include?(nil)
+        return nil if [node["name"], node["address"], node["review_info"], node["id"]].include?(nil)
         new(node)
       end
 
@@ -15,7 +15,7 @@ module Providers
         full_address = node["address"]
         @address = [full_address["street"], full_address["city"], full_address["state"], full_address["postal_code"]].join(" ")
         @reference = node["id"]
-        @average_rating = node["rating"]
+        @average_rating = node['review_info']['overall_review_rating']
       end
     end
   end
