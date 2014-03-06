43f423737fead161f18403030018c76fe863dbb0
Anika Reynolds & Richard Nuno <dev+anika+richard@unpakt.com>
2014-03-06 17:24:25 -0500

    Looks like Google has a flat rating for each review.

--- lib/providers/google/review.rb
+++ lib/providers/google/review.rb
@@ -4,20 +4,10 @@ module Providers
       attr_accessor :rating, :author_name, :reviewed_at, :comment
 
       def initialize(node)
-        @rating = average_rating(node["aspects"])
+        @rating = node["rating"]
         @author_name = node["author_name"]
         @reviewed_at = Time.at(node["time"].to_i)
         @comment = node["text"]
-      end
-
-      private
-
-      def average_rating(arr)
-        return 0 if arr.nil? || arr.size < 1
-
-        sum = 0
-        arr.each { |el| sum += el["rating"].to_f }
-        sum.to_f / arr.size
       end
     end
   end
